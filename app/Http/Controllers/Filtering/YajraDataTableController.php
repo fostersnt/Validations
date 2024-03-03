19a5ada9fb704b257fd53e3fa271d8b413c43151
= <=>
2024-03-03 11:08:30 +0000

    Added a form

--- app/Http/Controllers/Filtering/YajraDataTableController.php
+++ app/Http/Controllers/Filtering/YajraDataTableController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers\Filtering;
 use App\DataTables\TableOneDataTable;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class YajraDataTableController extends Controller
 {
-    public function index(TableOneDataTable $dataTable)
+    public function index(Request $request, TableOneDataTable $dataTable)
     {
+        Log::info("\nSTART DATE: " . $request->start_date);
+        Log::info("\nEND DATE: " . $request->end_date);
+        Log::info("\CATEGORY DATE: " . $request->end_date);
         return $dataTable->render('table_one');
     }
 }
--- resources/views/table_one.blade.php
+++ resources/views/table_one.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
     <div class="table table-responsive">
+        <form action="">
+            <div class="row">
+                <div class="col-md-4">
+                    <label for="">Start Date</label>
+                    <input class="form-control" type="text" name="start_date" id="start_date">
+                </div>
+                <div class="col-md-4">
+                    <label for="">End Date</label>
+                    <input class="form-control" type="text" name="end_date" id="end_date">
+                </div>
+                <div class="col-md-4">
+                    <label for="">Category</label>
+                    <input class="form-control" type="text" name="category" id="category">
+                </div>
+            </div>
+        </form>
         {{ $dataTable->table() }}
     </div>
 @endsection
